fa99cf136a8b5582474b583caf22116057437387
Jesse Fender <jf0772256@otc.edu>
2017-02-13 02:38:28 -0600

    ignore these changes.
    nothing here but testing

--- unused/index.php
+++ unused/index.php
@@ -1,6 +1,9 @@
 <?php
   include_once("../model/database.php");
   include_once("../model/Users.php");
+  include_once("../model/Admin.php");
+  include_once("../model/MailMessages.php");
+  include_once("../model/Posts.php");
 
   //temp include the tables.php from the installer to test some functions...
   //include_once("../administration/database/tables.php");
@@ -16,13 +19,14 @@
   // //user_showDashboard();
  ?>
 
- <!DOCTYPE html>
- <html>
-   <head>
-     <meta charset="utf-8">
-     <title>debug.php</title>
-   </head>
-   <body>
-     
-   </body>
- </html>
+<?php
+include("../partial/_head.php");
+include("../partial/user/_options.php");
+
+//include("../partial/user/_message_list.php");
+include("../partial/user/_new_message.php");
+//include("../partial/user/_read_message.php");
+?>
+<?php
+include("../partial/_pageend.php");
+?>
